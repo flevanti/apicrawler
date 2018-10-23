136b23ad301b3b016c438c72fd58bdc1e0a96a3c
Francesco Levanti <francesco@levanti.net>
2018-10-23 18:19:16 +0100

    shell script to run app in lambda docker container now creates the network if not present. that is the same network used by the mongo configuration. When mongo is running the network is present but if mongo is not running the network is not available and the container was failing to start. now.. no matters who is started before.. they both work... 🚀

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ./compile.sh
+echo "CREATING DOCKER NETWORK IF IT DOESN'T EXIST 🤔 "
+docker network inspect mongonetwork > /dev/null 2>&1 || docker network create mongonetwork > /dev/null
 echo RUNNING DOCKER COMMAND 🐳
 docker run --network mongonetwork --rm -v "$PWD":/var/task lambci/lambda:go1.x apicrawler "$(< dummyPayload.json)" 
